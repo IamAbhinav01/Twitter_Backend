88eae6c22c86d3303db7b0d0313086fbe4e1cdf7
Abhinav <AbhinavSunil@hotmail.com>
2026-01-21 13:41:37 +0530

    stop tracking database config

--- src/config/database.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const connect = async () => {
-  await mongoose.connect(
-    'mongodb+srv://abhinavsunil_db_user:abhinav@cluster0.rbgfyxs.mongodb.net/'
-  );
-};
-
-export default connect;
